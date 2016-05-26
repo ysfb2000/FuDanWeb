d281279cd57bcbd2b1b624ffa93d4c3ab7a574b2
XJL Xu <tkzs2000@hotmail.com>
2016-05-26 19:00:01 +0800

    sp0.2

---
+++ Web/Js/Common.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $("button").mousedown(function () {
+        $(this).css("position", "relative").css("left","2px").css("top","2px");
+    });
+
+    $("button").mouseup(function () {
+        $(this).css("position", "static");
+    });
+
+});
+
